b7baf89a175dc59f8ac1aecce6b12e2c5c6be0e3
nikhil174 <nikhilsrivastavaone@gmail.com>
2022-04-20 15:15:07 +0530

    added frontend UI

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -8,12 +8,14 @@ const {
   deleteGoal,
 } = require("../controllers/goalController");
 
+const { protect } = require("../middleware/authMiddleware");
+
 // router.get("/", getGoals);
 // router.post("/", setGoal);
 // router.put("/:id", updateGoal);
 // router.delete("/:id", deleteGoal);
 
-router.route("/").get(getGoals).post(setGoal);
-router.route("/:id").put(updateGoal).delete(deleteGoal);
+router.route("/").get(protect, getGoals).post(protect, setGoal);
+router.route("/:id").put(protect, updateGoal).delete(protect, deleteGoal);
 
 module.exports = router;
